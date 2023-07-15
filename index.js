7b0321f97312c63d56f6e99fb53e7b5df943c9a4
Yasmin Castillo <yasmin_c@outlook.com>
2023-07-15 11:29:07 -0500

    added package.json data

---
+++ index.js
@@ -0,0 +1,37 @@
+const inquirer = require('inquirer');
+const generateSVGLogo = require('./examples/');
+const fs = require('fs');
+
+
+const promptUser = () =>{
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name : "characters",
+                message: "What text? Enter up to 3 characters.",
+            },
+            {
+                type: 'input',
+                name : "color",
+                message: "What color?",
+            },
+            {
+                type: 'list',
+                name : "shape",
+                message: (["Circle" , "Square" , "Triangle"]),
+            },
+
+        ])
+        .then(res => {
+            writeToFile(res)
+        })
+        
+    };
+
+    const init = () => {
+        promptUser();
+    
+    }
+
+    init();
